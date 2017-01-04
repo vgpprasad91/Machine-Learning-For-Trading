642bc672c3fa99949925aaf3e7b80b2842353495
Guruprasad Venkataraghavan <vguruprasad91@gmail.com>
2017-01-04 14:35:14 -0500

    built a clean dataframe for only the SPY

---
+++ 2._working_with_multiple_stocks/_2.2_attach_stock_data_in_empty_df.py
@@ -0,0 +1,28 @@
+
+# write a function to read the empty dataframe
+def test_run():
+  
+  # import pandas
+  import pandas as pd
+  
+  # write a command to read in the date_range
+  start_date = "2010-01-22"
+  end_date = "2010-01-26"
+  dates = pd.date_range(start_date, end_date)
+  
+  # create an empty dataframe with the date_range as the index
+  df = pd.DataFrame(index=dates)
+  
+  # Read the SPY data into a temporary dataframe
+  dfSPY = pd.read_csv("data/SPY.csv",index_col="Date",parse_dates=True, usecols=['Date','Adj Close'],na_values=['nan'])
+  
+  # Join the two dataframes using DataFrame.join()
+  df = df.join(dfSPY) # dataframe.join() does a left join by default
+  
+  # Drop the rows where it is na
+  df = df.dropna()
+  print df
+
+# write a command to read in the main funcion
+int __name__ == "main":
+  test_run()
