4d484dc872fd651ce87dda90291b63256dc2fdf5
Guruprasad Venkataraghavan <vguruprasad91@gmail.com>
2017-01-03 21:58:37 -0500

    wrote a code to print the mean volume

--- 1. Reading and Plotting Stock Data/1.3_calculate_mean_value_for_each_stock.py
+++ 1. Reading and Plotting Stock Data/1.3_calculate_mean_value_for_each_stock.py
@@ -1,4 +1,20 @@
-# 
+# write a function to get the mean volume
+def get_mean_volume(symbol):
+  
+  # import pandas
+  import pandas as pd
+  
+  # write a command to get the data into a dataframe
+  df = pd.read_csv('data/{}.csv'.format(symbol))
+  
+  # return the mean volume of the stocks
+  return df['Volume'].mean()
+
+# write a function to pass the symbols for finding the mean
+def test_run():
+  for symbol in ['AAPL','Goog']:
+    print 'mean volume:'
+    print symbol, get_mean_volume(symbol)
 
 
 # write a main command to run standalone
