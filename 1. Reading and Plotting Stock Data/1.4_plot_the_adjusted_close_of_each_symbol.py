00324e9b97831ba38a9c00aea3cfd4a9cbbdd9ad
Guruprasad Venkataraghavan <vguruprasad91@gmail.com>
2017-01-03 22:25:49 -0500

    wrote a script to print the adjusted close column

--- 1. Reading and Plotting Stock Data/1.4_plot_the_adjusted_close_of_each_symbol.py
+++ 1. Reading and Plotting Stock Data/1.4_plot_the_adjusted_close_of_each_symbol.py
@@ -1,1 +1,24 @@
 
+def plot_adj_close(symbol):
+  
+  # import pandas
+  import pandas as pd
+  
+  # import matplotlib to plot values
+  import matplotlib.pyplot as plt
+  
+  # write a command to read the data in a dataframe
+  df = pd.read_csv('data/{}.csv'.format(symbol))
+  
+  ## write a command to plot the adjusted close column
+  df['Adj Close'].plot()
+  plt.show()
+
+# write a function to get all the symbols
+def test_run():
+  for symbol in ['AAPL','Goog']:
+    plot_adj_close(symbol)
+
+# write the main command to run the function
+int __name__ == "main":
+  test_run()
