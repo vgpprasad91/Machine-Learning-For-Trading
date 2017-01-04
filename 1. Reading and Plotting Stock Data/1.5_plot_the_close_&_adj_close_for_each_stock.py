1594384950a4690cd56fe713a933a5c2d9f6e43d
Guruprasad Venkataraghavan <vguruprasad91@gmail.com>
2017-01-03 22:43:49 -0500

    wrote code to plot two columns in a single chart

--- 1. Reading and Plotting Stock Data/1.5_plot_the_close_&_adj_close_for_each_stock.py
+++ 1. Reading and Plotting Stock Data/1.5_plot_the_close_&_adj_close_for_each_stock.py
@@ -1,1 +1,25 @@
 
+# write a code to plot the close and adj close columns
+def plot_close_adj_close(symbol):
+  
+  # import pandas
+  import pandas as pd
+  
+  # import matplotlib
+  import matplotlib.pyplot as plt
+  
+  # write a code to read the data into the dataframe
+  df = pd.read_csv('data/{}.csv'.format(symbol))
+  
+  # write code to plot close and adj close columns for each symbol
+  df[['close','Adj Close']].plot()
+  plt.show()
+
+# write a code to get each symbol from stocks list
+def test_run():
+  for symbol in ['AAPL','Goog']:
+    plot_close_adj_close(symbol)
+
+# write a code to activate the main function
+int __name__ == "main":
+  test_run()
