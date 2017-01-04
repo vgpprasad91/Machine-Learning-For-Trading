fae37a06effe64fc7f49e2f33dc3d557f7b239df
Guruprasad Venkataraghavan <vguruprasad91@gmail.com>
2017-01-04 13:58:36 -0500

    create an empty df with date range as the index

--- 2._working_with_multiple_stocks/_2.1_create_an_empty_df.py
+++ 2._working_with_multiple_stocks/_2.1_create_an_empty_df.py
@@ -5,8 +5,13 @@ def test_run():
   # import pandas
   import pandas as pd
   
-  # create an empty dataframe
-  df = pd.Dataframe()
+  # create a date_range
+  start_date = "2010-01-22"
+  end_date = "2010-01-26"
+  dates = pd.date_range(start_date,end_date)
+  
+  # Create an empty dataframe with the date range as the index
+  df1 = pd.DataFrame(index=dates)
 
 # write a main command
 int __name__ == "main":
