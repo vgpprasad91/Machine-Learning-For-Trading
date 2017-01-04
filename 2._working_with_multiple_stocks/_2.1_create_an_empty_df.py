436154fd17147e13270f56e29df6b9c571477bd7
Guruprasad Venkataraghavan <vguruprasad91@gmail.com>
2017-01-04 13:51:10 -0500

    Update _2.1_create_an_empty_df.py

--- 2._working_with_multiple_stocks/_2.1_create_an_empty_df.py
+++ 2._working_with_multiple_stocks/_2.1_create_an_empty_df.py
@@ -1,1 +1,13 @@
 
+# write a function to create an empty dataframe
+def test_run():
+  
+  # import pandas
+  import pandas as pd
+  
+  # create an empty dataframe
+  df = pd.Dataframe()
+
+# write a main command
+int __name__ == "main":
+  test_run()
