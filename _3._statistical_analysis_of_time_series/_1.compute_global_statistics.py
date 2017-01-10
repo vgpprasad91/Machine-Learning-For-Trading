b73cbc0d3e0316419030d80df966e774caade42c
Guruprasad Venkataraghavan <vguruprasad91@gmail.com>
2017-01-09 20:22:15 -0500

    wrote code for computing global statistics

--- _3._statistical_analysis_of_time_series/_1.compute_global_statistics.py
+++ _3._statistical_analysis_of_time_series/_1.compute_global_statistics.py
@@ -1,2 +1,18 @@
+
+
+# write a test run function to get the symbols
+def test_run():
+  # Read data
+  dates = pd.date_range("2010-01-01","2012-12-31")
+  symbols = ["Goog", "SPY", "XOM", "GLD"]
+  df = get_data(symbols, dates)
+  plot_data(df)
+  
+  # Compute global statistics
+  print df.mean()
+  print df.median()
+  print df.std()
+
 # write a int main function to pass the main function
 int __name__ == "main":
+  test_run()
